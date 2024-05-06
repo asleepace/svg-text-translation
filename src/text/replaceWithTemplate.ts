f66b49c31ea0b3c03135c98fa2f27a4562d895e1
Colin <colin_teahan@yahoo.com>
2024-05-06 15:01:54 -0700

    cleaning up the code

--- src/text/replaceWithTemplate.ts
+++ src/text/replaceWithTemplate.ts
@@ -19,7 +19,7 @@ export type ReplacementOptions = Partial<{
 export function replaceWithTemplate(svg: string, lines: string[], options: ReplacementOptions = {}): string {
   const { y = 15, fontSize = 100, lineHeight = 5 } = options
 
-  const spans = lines.map((line, index) => `<tspan x="50%" y="${y + (index * lineHeight)}%">${line}</tspan>`).join("\n")
+  const spans = lines.filter(Boolean).map((line, index) => `<tspan x="50%" y="${y + (index * lineHeight)}%">${line}</tspan>`).join("\n")
   const template = `<text x="50%" y="50%" letter-spacing="-3.5px" font-size="${fontSize}" text-anchor="middle" dominant-baseline="middle" fill="#111111" font-family="Inter" font-weight="bold">${spans}</text>`
 
   return svg.replace(SVG_TEXT_REGEX, template)
